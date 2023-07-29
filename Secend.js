8eea7afa4a599fcacd92e69d6141354c7bec236e
pouria kiani <pouria.kiani7@gmail.com>
2023-07-29 21:38:44 +0330

    first commit

---
+++ Secend.js
@@ -0,0 +1,331 @@
+// برنامه ای بنویسید که 2 عدد از کاربر دریافت کرده و عملیات ضرب، تقسیم، جمع و تفریق را روی آن ها پیاده سازی کنید
+////////////////////////////////
+// adad haye daryafti ra ** % + - / * rosh anjam dahid
+// let number1 = 10;
+// let number2 = 2;
+// // adad hai k az karbar gereftim
+// let numbers = number1 + number2; // 10+2=12
+// // console.log(numbers);
+// numbers = number1 - number2; // 2-10=8
+// // console.log(numbers);
+// numbers = number1 * number2; //10*2=20
+// // console.log(numbers);
+// numbers = number1 ** number2;
+// // console.log(numbers);
+// numbers = number1 / number2; //10/2=5
+// // console.log(numbers);
+// number1 = 19;
+// number2 = 4;
+// numbers = number1 ** number2; //19*19*19*19=130321 (power/tavan)
+// console.log(numbers);
+// numbers = number1 / number2; //19/4=4.75
+// console.log(numbers);
+// numbers = number1 % number2; //19%4=3
+// console.log(numbers);
+/////////////////////////////////////////
+// let text1 = "welcome";
+// let text2 = "brother";
+// let textconcat1 = text1 + " " + text2;
+// console.log(textconcat1);
+// let textsConcat2 = text1 + " " + "\n" + text2;
+// console.log(textsConcat2);
+// let str1 = "hello";
+// let str2 = "world";
+// let strs = "hello" + " " + "world";
+// let strs = {
+//   strtext1: "hello",
+//   strtext2: "world",
+//   strstexs: "hello" + " " + "world",
+// };
+// console.log(strs);
+// let myName = "pouria";
+// let familyName = "kiani";
+// let myJob = "'back-end Developer'";
+// let fullName = myName + " " + familyName;
+// let pouria = {
+//   name: "pouria",
+//   familyName: "kiani",
+//   job: "Back-End Developer",
+// };
+// console.log(
+//   strs.strstexs +
+//     " " +
+//     "My name is:" +
+//     " " +
+//     pouria.name +
+//     pouria.familyName +
+//     "\n" +
+//     "and my job is:" +
+//     "\n" +
+//     " " +
+//     pouria.job
+// );
+// console.log(strs.strstexs);
+//{}===>curly brace
+//[]===> bracket
+//,===>kama
+//;===>semicolon
+//////////////////////////////////////////////////////
+
+// array||araye ==> practice
+// syntax array|| araye ==> let arrayName = []
+
+// menu sobhane ye restoran bakhshe sobhanasho ba araye benevisid va az khode araye + sobhane mexici khoroji begirid!!!
+
+// let breakFastMenu = ["irani", "englisi", "mexici", "chai"];
+
+// console.log(breakFastMenu);
+
+// console.log(breakFastMenu[2]);
+
+// menu restoran ra bakhshe nahar va sham ra ba araye benevisid va az khode araye + tabei khoroji begirid!!!
+
+// let mainMenu = [
+//   "polo morgh",
+//   "gheyme nesar",
+//   "ghorme sabzi",
+//   "gheyme",
+//   "kobide",
+//   "tabei",
+//   "kobide negini",
+// ];
+
+// console.log(mainMenu);
+
+// console.log(mainMenu[5]);
+
+// yek list az masshin haye mojod dar namayeshgah ra ba araye benevisid va az list koli va az audi khoroji begirid!!!
+
+// let cars = [
+//   "bmw",
+//   "benz",
+//   "nissan",
+//   "toyota",
+//   "maserati",
+//   "audi",
+//   "ferrari",
+//   "bugatti",
+// ];
+// console.log(cars);
+
+// console.log(cars[5]);
+/////////////////////////////////////////////////
+
+// let num1 = 12;
+
+// let num2 = 3;
+
+// let num3 = 4;
+
+// console.log(num1 / num3 == num2);
+
+// let srt1 = "12";
+
+// let srt2 = "3";
+
+// let srt3 = "4";
+
+// console.log(srt3 * num2 == num1);
+///////////////////////////////////////////////////////
+
+// let num1 = 12;
+
+// let num2 = 4;
+
+// let num3 = 3;
+
+// if (num2 / num3 === num1) {
+//   console.log("dorost ast");
+// } else {
+//   console.log("eshtebah ast");
+// }
+// let num1 = 12;
+
+// let num2 = 4;
+
+// let num3 = 3;
+
+// if (num2 * num2 == num1) {
+//   console.log("doros ast");
+// } else {
+//   console.log("eshtebah ast");
+// }
+// if (num1 / num1 == num1) {
+//   console.log("dorost ast");
+// } else {
+//   console.log("eshtebah ast");
+// }
+// if (num2 * num3 == num1) {
+//   console.log("dorost ast");
+// } else {
+//   console.log("eshtebah ast");
+// }
+// if (num1 / num1 == 1) {
+//   console.log("sahih ast");
+// } else {
+//   console.log("eshtebah ast");
+// }
+// if (num1 / 3 === num3) {
+//   console.log("sahih ast");
+// } else {
+//   console.log("sahih nemibashad");
+// }
+// if (num1 * num1 == 144) {
+//   console.log("bale doroste");
+// } else {
+//   console.log("kamelan eshtebahe");
+// }
+// if (num3 ** num3 == num1) {
+//   console.log("bale sahih ast");
+// } else {
+//   console.log("'Afarin' kamelan eshtebahe :)");
+// }
+
+// let num1 = 14;
+// let num2 = 2;
+
+// if (num1 % num2 === 0) {
+//   // 14 % 2 = 0      (%==> baghi mande)(/==>kharej ghesmate taghsim)
+//   console.log("zoj");
+// } else {
+//   console.log("fard");
+// }
+
+// let num1 = 115;
+// let num2 = 216;
+
+// if (num1 % 2 === 0) {
+//   console.log("عدد مورد نظر زوج می باشد");
+// } else {
+//   console.log("عدد مورد نظر فرد می باشد");
+// }
+// if (num2 % 2 === 0) {
+//   console.log("عدد مورد نظر زوج می باشد");
+// } else {
+//   console.log("عدد مورد نظر فرد می باشد");
+// }
+
+// let firstUsserNumber = "15";
+// let secendUsserNumber = "15";
+// let thirdUsserNumber = "15";
+
+// let avg = (+firstUsserNumber + +secendUsserNumber + +thirdUsserNumber) / 3;
+// console.log(avg);
+
+// function quartercal(number1, number2) {
+//   let point1 = number1 * 2;
+//   let point2 = number2 * 3;
+//   let sum = point1 + point2;
+//   console.log(sum);
+// }
+// quartercal(38, 8);
+
+// function stringConcatenation(str) {
+//   const defaultString = "Edabit";
+//   let concatStrings = `(${defaultString} ${str})`;
+//   console.log(concatStrings);
+// }
+// stringConcatenation("zaban");
+
+// function isSumLess(number1, number2, number3) {
+//   let sumNumbers = number1 + number2;
+//   if (sumNumbers <= number3) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// isSumLess(10, 20, 35);
+// isSumLess(50, 50, 150);
+// isSumLess(50, 100, 50);
+
+// function sumAllNumbers(number) {
+//   let usserNumber = number;
+//   let sumnumbers = 0;
+//   for (let i = 0; i <= number; i++) {
+//     sumnumbers = sumnumbers + i;
+//     console.log(i);
+//   }
+
+//   console.log(sumnumbers);
+// }
+// sumAllNumbers(4);
+
+// function allHousesUsser(number) {
+//   const updateHouses = number - 1;
+//   const sumNumber = updateHouses * 5 + 6;
+//   console.log(sumNumber);
+// }
+// allHousesUsser(200);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// let userAge1 = "25";
+
+// let userAge2 = "17";
+
+// if (userAge1 < 18) {
+//   console.log("سن شما برای ورود مجاز نمی باشد");
+// } else {
+//   console.log("به پرن هاب خوش امدید");
+// }
+// if (userAge2 < 18) {
+//   console.log("سن شما برای ورود مجاز نمی باشد");
+// } else {
+//   console.log("به پرن هاب خوش امدید");
+// }
+// !asdasdasd
+// ?adasdadasd
+// *asdasdasd
+
+// let userAge = +prompt("سن خود را وارد کنید");
+
+// if (userAge >= 18) {
+//   console.log("wellcom");
+// } else {
+//   console.log("access denied");
+// }
+
+///////////////////////////////////////////////////////////
+
+// let userScoure = +prompt("nomreye khod ra vared konid");
+// if (userScoure >= 18 && userScoure <= 20) {
+//   console.log("perfect");
+// } else if (userScoure >= 15 && userScoure <= 17) {
+//   console.log("very good");
+// } else if (userScoure >= 10 && userScoure <= 14) {
+//   console.log("Good");
+// } else if (userScoure >= 5 && userScoure <= 9) {
+//   console.log("poor");
+// } else {
+//   console.log("out of range");
+// }
+
+// let userInputNumber = +prompt("adad morede nazar ra vared konid");
+// if (userInputNumber % 2 === 0) {
+//   console.log("adad zoj mibashad");
+// } else {
+//   console.log("adad fard mibashad");
+// }
+// userInputNumber =
+//   userInputNumber % 2 === 0
+//     ? console.log("adad vared shode zoj ast")
+//     : console.log("adad fard mibashad");
+
+// method haye math
+// console.log(Math.round(14.5));
+// console.log(Math.ceil(14.4));
+// console.log(Math.floor(14.4));
+// console.log(Math.trunc(14.4));
+// console.log(Math.pow(10, 2));
+// console.log(Math.min(10, 2, 500, 900, 1, 1, 3));
+// console.log(Math.max(10, 2, 500, 900, 1, 3));
+// console.log(Math.abs(-30));
+// console.log(Math.floor(Math.random() * 10));
+
+// let capchaTemplate = Math.floor(Math.random() * 10000);
+// console.log(capchaNumber);
+
+// let capchaNumber = +prompt(`olgoe adadi ra vared namaid : ${capchaTemplate}`);
+
+// capchaNumber === capchaTemplate ? console.log("its ok") : console.log("not ok");
